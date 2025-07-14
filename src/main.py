84f65d411e7a43118a9a26a8382c39b127a489b6
nocatchynamehere <wilson.whisnant@gmail.com>
2025-07-14 02:46:19 -0400

    laying some groundwork

--- src/main.py
+++ src/main.py
@@ -1,1 +1,7 @@
-print("hello world")
+from textnode import TextNode, TextType
+
+def main():
+    node = TextNode("this is some anchor text", TextType.LINK, "https://www.boot.dev")
+    print(node)
+
+main()
--- src/textnode.py
+++ src/textnode.py
@@ -9,15 +9,18 @@ class TextType(Enum):
     IMAGE = "image"
 
 class TextNode:
-    def __init__(self, text, text_type=TextType, url=None):
+    def __init__(self, text, text_type, url=None):
         self.text = text
         self.text_type = text_type
         self.url = url
 
     def __eq__(self, target):
-        if self == target:
-            return True
-        return False
+        return (
+            isinstance(target, TextNode) and
+            self.text == target.text and
+            self.text_type == target.text_type and
+            self.url == target.url
+        )
     
     def __repr__(self):
         return f'TextNode({self.text}, {self.text_type.value}, {self.url})'
